84cc35d11b083656f383cfb339ddf61695d4a8cc
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2026-02-10 09:04:35 +0100

    Mise à jour de la définition de l'URL de l'instance PocketBase pour inclure la variable d'environnement process.env.PUBLIC_POCKETBASE_URL

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -3,7 +3,9 @@ import PocketBase from "pocketbase";
 
 // Définition de l'URL de l'instance PocketBase
 const POCKETBASE_URL =
-  import.meta.env?.PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090";
+  process.env.PUBLIC_POCKETBASE_URL ||
+  import.meta.env?.PUBLIC_POCKETBASE_URL ||
+  "http://127.0.0.1:8090";
 
 // Création de l'instance PocketBase avec l'URL définie plus haut
 const pb = new PocketBase(POCKETBASE_URL);
