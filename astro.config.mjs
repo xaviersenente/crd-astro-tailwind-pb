a7a018e973fa62e6293c35ad99426ca4e9fdd63e
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2026-02-16 13:49:17 +0100

    Mise à jour des URLs du site dans la configuration et le fichier robots.txt pour refléter le nouveau domaine.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://conservatoire-belfort.fr",
+  site: "https://conservatoirecrd.netlify.app",
 
   vite: {
     plugins: [tailwindcss()],
